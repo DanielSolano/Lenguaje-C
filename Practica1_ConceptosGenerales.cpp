1541d98e75cfe82f1eef57b0eca94c24f8725549
DanielSolano <solanomezaangel@gmail.com>
2023-08-09 16:03:33 -0700

    Codigo

---
+++ Practica1_ConceptosGenerales.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int limite;
+int resultado=0;
+
+printf("Ingrese un numero entero positivo:");
+scanf("%d",&limite);
+for (int i=1;i<=limite;i++)
+{
+    resultado +=1;
+}
+
+printf("La suma de los primeros %d numeros es: %d\n",limite, resultado);
+return 0;
+}
