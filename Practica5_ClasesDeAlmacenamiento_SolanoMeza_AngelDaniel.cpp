2dab821f67b3b47362678923edaf3624e789d91b
DanielSolano <solanomezaangel@gmail.com>
2023-09-30 22:13:41 -0700

    modified:   Practica5_ClasesDeAlmacenamiento_SolanoMeza_AngelDaniel.cpp modified:   output/Practica5_ClasesDeAlmacenamiento_SolanoMeza_AngelDaniel.exe

--- Practica5_ClasesDeAlmacenamiento_SolanoMeza_AngelDaniel.cpp
+++ Practica5_ClasesDeAlmacenamiento_SolanoMeza_AngelDaniel.cpp
@@ -1,11 +1,18 @@
+/*
+Nombre del archivo: Practica5_ClasesDeAlmacenamiento_SolanoMeza_AngelDaniel.cpp.c
+Autor: Solano Meza Angel Daniel
+Fecha de creación: 30 de septiembre de 2023
+Descripción: Este archivo contiene todo el contenido de la practica 5
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 //*** PROTOTIPOS DE FUNCIONES  ******
 int msges();
 void menu();
 void Loteria(void);
-void Velocidad(void);
+void iniciarCarrera(void);
 int generarNumeroLoteria(void);
 //****  main principal  *********
 int main()
@@ -23,7 +30,7 @@ int msges()
     system("CLS");
     printf("   M  E   N   U \n");
     printf("1.- LOTERIA \n");
-    printf("2.- VELOCIDAD \n");
+    printf("2.- CARRERAS \n");
     printf("0.- SALIR  \n");
     printf("ESCOGE UNA OPCION: ");
     scanf("%d", &op);
@@ -43,7 +50,7 @@ void menu()
             Loteria();
             break;
         case 2:
-            Velocidad();
+            iniciarCarrera();
             break;
         }
 
@@ -51,6 +58,10 @@ void menu()
 }
 
 //*********************
+/*
+Función: Loteria
+Descripción: Esta funcion realiza el juego de la loteria.
+*/
 void Loteria(void)
 {
     //  VARIALES LOCALES
@@ -59,7 +70,7 @@ void Loteria(void)
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
     printf("   LOTERIA\n");
-    numero =  generarNumeroLoteria();
+    numero = generarNumeroLoteria();
     if (numero == ganador)
     {
         printf("¡Felicidades, ganaste la lotería!\n");
@@ -72,16 +83,58 @@ void Loteria(void)
 }
 
 //*************************
-void Velocidad(void)
+/*
+Función: iniciarCarrera
+Descripción: Esta funcion hace correr 3 carros y te dice quen gano.
+*/
+void iniciarCarrera(void)
 {
 
     //  VARIALES LOCALES
+    int register Checo = rand() % 101 + 100;
+    int register Max = rand() % 101 + 100;
+    int register Lewis = rand() % 101 + 100;
+    float tiempo1, tiempo2, tiempo3;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   FACTORIAL\n");
+    printf("    CARRERAS\n");
+    printf("UN CUARTO DE MILLA A RECORRER\n");
+    sleep(1);
+    printf("        3\n");
+    sleep(1);
+    printf("        2\n");
+    sleep(1);
+    printf("        1\n");
+    sleep(1);
+    tiempo1 = (402 + .1) / Checo;
+    tiempo2 = (402 + .1) / Max;
+    tiempo3 = (402 + .1) / Lewis;
+    if ((tiempo1 < tiempo2) && (tiempo1 < tiempo2))
+    {
+        printf("\n    GANADOR: CHECO \n");
+    }
+    if ((tiempo2 < tiempo3) && (tiempo2 < tiempo1))
+    {
+        printf("\n    GANADOR: MAX \n");
+    }
+    if ((tiempo3 < tiempo2) && (tiempo3 < tiempo1))
+    {
+        printf("\n    GANADOR: LEWIS \n");
+    }
+    sleep(1);
+    printf("\nPILOTO: Checo\nVELOCIDAD: %d\nTIEMPO: %f\n", Checo, tiempo1);
+    sleep(1);
+    printf("\nPILOTO: Max\nVELOCIDAD: %d\nTIEMPO: %f\n", Max, tiempo2);
+    sleep(1);
+    printf("\nPILOTO: Lewis\nVELOCIDAD: %d\nTIEMPO: %f\n", Lewis, tiempo3);
     system("PAUSE");
 }
 //****************************
+/*
+Función: generarNumeroLoteria
+Descripción: Esta funcion genera un numero de loteria para compararlo con la variable estatica de la funcion Loteria.
+Valor de retorno: Un numero aleatorio entre 1 y 100.
+*/
 int generarNumeroLoteria(void)
 {
     //  VARIALES LOCALES
